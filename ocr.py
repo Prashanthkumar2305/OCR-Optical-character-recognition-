3c8620506d11cb6c83be4f93239abe33e8950507
PrashanthKumar K <96175181+Prashanthkumar2305@users.noreply.github.com>
2024-11-05 17:38:09 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-import pandas as pd
-
-
-# In[2]:
-
 
 import pandas as pd
 import pytesseract
@@ -16,13 +6,12 @@ from PIL import Image
 import requests
 from io import BytesIO
 import time
-from concurrent.futures import ThreadPoolExecutor ,as_completed
 
 
 # In[3]:
 
 
-get_ipython().run_cell_magic('time', '', 'df1=pd.read_csv(r"C:\\Users\\mmmm\\Desktop\\new\\photo_file.csv")\n')
+df1=pd.read_csv(r"C:\\Users\\mmmm\\Desktop\\new\\photo_file.csv")
 
 
 # In[4]:
@@ -52,7 +41,30 @@ df1['source_invoice_link']
 # In[9]:
 
 
-get_ipython().run_cell_magic('time', '', 'import pandas as pd\nimport requests\nfrom PIL import Image\nfrom io import BytesIO\nimport pytesseract\ncount=0\nfor url in df1[\'source_invoice_link\']:\n    try:\n        response=requests.get(url)\n        if response.status_code==200:\n            image=Image.open(BytesIO(response.content))\n            image.convert("L")\n            text=pytesseract.image_to_string(image)\n            count+=1\n            print(count)\n            \n            if \'apmc yard\' in text.lower() or \'apmc\' in text.lower():\n                df1.loc[df1[\'source_invoice_link\']==url,\'apmc_check\']=\'Yes\'\n            else:\n                df1.loc[df1[\'source_invoice_link\'] == url, \'apmc_check\']="No"\n        else:\n            print(f"We\'re getting error because of this {url} status_code is {response.status_code}")\n    except Exception as e:\n        print(f"error occuring {url}: {e}")\n')
+%%time
+import pandas as pd
+import requests
+from PIL import Image
+from io import BytesIO
+import pytesseract
+count=0
+for url in df1['source_invoice_link']:
+  try:
+  response=requests.get(url)
+  if response.status_code==200:
+    image=Image.open(BytesIO(response.content))
+    image.convert("L")
+    text=pytesseract.image_to_string(image)
+    count+=1
+    print(count)
+    if 'apmc yard' in text.lower() or 'apmc' in text.lower():
+      df1.loc[df1['source_invoice_link']==url,'apmc_check']='Yes'
+    else:
+      df1.loc[df1['source_invoice_link'] == url, 'apmc_check']="No"
+  else:
+  print(f"We're getting error because of this {url} status_code is {response.status_code}")
+  except Exception as e:
+    print(f"error occuring {url}: {e}")')
 
 
 # In[12]:
